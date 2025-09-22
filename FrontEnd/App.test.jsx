0449f8628b5a5135c58dace29063bfe18db2ab3d
Dhineshkumar MM <126142586+DhineshDine@users.noreply.github.com>
2025-09-22 20:13:25 +0530

    Update App.test.jsx

--- FrontEnd/App.test.jsx
+++ FrontEnd/App.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { describe, test, expect } from 'vitest';
-import App from './App';
+import App from './App.jsx';
 
 describe('App component', () => {
   test('renders learn react text', () => {
