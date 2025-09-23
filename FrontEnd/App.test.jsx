c6fd07eacbdb00be189b76914e4961c97b8e98a2
Dhineshkumar MM <126142586+DhineshDine@users.noreply.github.com>
2025-09-23 18:20:17 +0530

    Update App.test.jsx

--- FrontEnd/App.test.jsx
+++ FrontEnd/App.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import App from "./src/App.jsx";
 import { MemoryRouter } from "react-router-dom";
+import { test, expect } from 'vitest';
 
 test("App component renders Login heading", () => {
   render(
